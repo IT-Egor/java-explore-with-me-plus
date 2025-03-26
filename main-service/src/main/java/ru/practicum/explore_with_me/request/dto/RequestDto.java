b3dcce96fb3b3b47849dd5625f9084c4184e6a96
DmitryGavrilovvv <mrfritherrr@yandex.ru>
2025-03-26 19:21:51 +0300

    Fix request mapping

--- main-service/src/main/java/ru/practicum/explore_with_me/request/dto/RequestDto.java
+++ main-service/src/main/java/ru/practicum/explore_with_me/request/dto/RequestDto.java
@@ -1,9 +1,17 @@
 package ru.practicum.explore_with_me.request.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestDto {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime created;
